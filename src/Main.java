2d2e25e3d05aa76bf6f8a33e6b59c481d20ff159
Mike <109431533+1111334@users.noreply.github.com>
2023-07-16 10:30:20 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        var myName = "Maicol";
+
+        if (myName.length() % 2 == 0) {
+
+            System.out.println("the set of letters in my name is even");
+
+        }else {
+            System.out.println("the set of letters in my name is odd");
+        }
+    }
+}
